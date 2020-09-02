bff41d7008204d0c4934dd199c001b9c0a1c3943
Igor <igorgronthebest@gmail.com>
2020-09-02 17:12:00 +0300

    started to create auth page

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import '../style/auth.less';
 
 export const Auth = () => {
 	
 	return (
-		<div>
-			<h2>AuthPage</h2>
+		
+		<div className="wrapper">
+			<form className="auth-form" action="/">
+				<h3 className="mb-3">Авторизация</h3>
+				<label>Логин: <input type="text" /></label>
+				<label>Пароль: <input type="password" /></label>
+			</form>
 		</div>
 	);
 }
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,21 +1,22 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from '../components/App';
 import Header from '../components/Header';
 import {useRoutes} from '../hooks/useRoutes.js';
 
 export default function Root ({store}) {
-	const routes = useRoutes(true);
+	const routes = useRoutes(false);
 	return (
 		<Provider store={store}>
 			<Header></Header>
-			<Fragment>
+			<Container>
 				<Router>
 					{routes}
 				</Router>
-			</Fragment>
+			</Container>
 		</Provider>
 	);
 }
